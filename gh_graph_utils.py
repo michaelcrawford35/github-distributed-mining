d7b476b05f0fb74454c1883156a0c3cf8268e867
michaelcrawford35 <mcrawbeast35@gmail.com>
2021-03-11 18:09:53 -0600

    Update gh graph methods. Remove pkl.

--- gh_graph_utils.py
+++ gh_graph_utils.py
@@ -1,21 +1,27 @@
 def augment_owns(graph, user, lang=False):
     graph.add_node(user.login, node_type='user')
     repos = user.get_repos()
-    for repo in repos:
+    repolist = []
+    if repos.totalCount == 0:
+        return []
+    for repo in repos[:200]:
         graph.add_node(repo.name, node_type='repo')
         graph.add_edge(user.login, repo.name, edge_type='owns')
+        repolist.append(repo)
         if lang:
             graph.add_node(repo.language, node_type='lang')
             graph.add_edge(user.login, repo.language, edge_type='users')
             graph.add_edge(repo.name, repo.language, edge_type='speaks')
-    return repos
+    return repolist
 
 def augment_stargazers(graph, repo):
     try:
         gazers = repo.get_stargazers()
-        for stargazer in gazers:
+        users = set()
+        for stargazer in gazers[:200]:
             graph.add_node(stargazer.login, node_type='user')
             graph.add_edge(stargazer.login, repo.name, edge_type='gazes')
-        return gazers
+            users.add(stargazer)
+        return users
     except: #It's fun to play at the... D   M  C  A
         return []
